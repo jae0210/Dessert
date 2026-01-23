2b5f8d07f8fc9d8778caea27ebd584e3b9919794
jae0210 <j9325222@gmail.com>
2026-01-23 18:18:41 +0900

    26.01.23 ui 적용

---
+++ Assets/J/J_Scripts/J_Customize/J_SwatchButtonUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class J_SwatchButtonUI : MonoBehaviour
+{
+    public Image colorFill;
+    public GameObject selectedGlow;
+    public GameObject checkMark;
+
+    public void SetColor(Color c)
+    {
+        if (colorFill) colorFill.color = c;
+    }
+
+    public void SetSelected(bool on)
+    {
+        if (selectedGlow) selectedGlow.SetActive(on);
+        if (checkMark) checkMark.SetActive(on);
+    }
+}
